1553ba4359a3fa18676d281baba8ebb57dc9c038
CamilaGonzalez97 <112906352+CamilaGonzalez97@users.noreply.github.com>
2022-11-08 19:15:52 -0300

    Update Asistencia Grupo Programacion UTN.py

--- Asistencia Grupo Programacion UTN.py
+++ Asistencia Grupo Programacion UTN.py
@@ -134,3 +134,48 @@ bicicleta = Bicicleta("Blanca", 2, "Montaña")
 print(vehiculo)
 print(camioneta)
 print(bicicleta)
+"""
+----------------------------------------------------------------------------------------------------
+"""
+#Ejercicio de Camila Gónzalez
+class FiguraGeometrica:
+    def __init__(self,ancho, alto):
+        self._ancho = ancho
+        self._alto = alto
+
+    @property
+    def ancho(self):
+        return self._ancho
+
+    @ancho.setter
+    def ancho(self,ancho):
+        self._alto = ancho
+
+    @property
+    def alto(self):
+        return self._alto
+
+    @alto.setter
+    def alto(self,alto):
+        self._alto = alto
+
+    def __str__(self):
+        return f'FiguraGeometrica [ Ancho : {self._ancho}, Alto: {self._alto}]'
+    
+    class Color:
+    def __init__(self,color):
+        self.color = color
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self,color):
+        self._color = color
+
+    def calcular_area(self):
+        return self.alto * self.ancho
+
+    def __str__(self):
+        return f'{FiguraGeometrica.__str__(self)} {Color.__str__(self)}'
