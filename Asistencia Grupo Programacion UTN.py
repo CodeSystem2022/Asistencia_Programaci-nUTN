6a152f12b6ec2bc2a07d2df5290f801cb434bdcd
Nadia Cruz <112596017+NadiaCruz1@users.noreply.github.com>
2022-11-08 11:41:56 -0300

    Update Asistencia Grupo Programacion UTN.py

--- Asistencia Grupo Programacion UTN.py
+++ Asistencia Grupo Programacion UTN.py
@@ -77,3 +77,60 @@ print(f'El área del rectángulo es: {area1.area():.2f}')
 """
 ----------------------------------------------------------------------------------------------------
 """
+class Vehiculo:
+    """
+    Ejercicio de Nadia Cruz
+    Definir una clase padre llamada Vehiculo y dos clases hijas llamadas
+    Auto y Bicicleta, las cuales heredaran de la clase padre Vehiculo.La clase
+    padre debe tener los siguientes atributos y métodos:
+    Vehiculo (clase padre)
+    -ATRIBUTOS(COLOR,RUEDAS)
+    -Métodos (__init__() y __str__())
+
+    Auto(clase hija de vehiculo)
+    -Atibutos( velocidad(km/hr))
+    -Métodos (__init__() y __str__())
+    Bicicleta(clase hija de Vehiculo)
+    -Atributos(tipo(urbana /montaña/etc.)
+    -Métodos (__init__() y __str__())
+    Crear un objeto de cada clase
+
+    """
+
+    # Creación clases y métodos
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+    def __str__(self):
+        return "Color: " + self.color + ", Ruedas: " + str(self.ruedas)
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return super().__str__()+ ', Velocidad(km/hr): ' + str(self.velocidad)
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return super().__str__()+ 'Tipo: ' + self.tipo
+
+
+# Creación objetos
+
+vehiculo = Vehiculo("Rojo", 5)
+
+camioneta = Auto("Negro", 4, 140)
+
+bicicleta = Bicicleta("Blanca", 2, "Montaña")
+
+print(vehiculo)
+print(camioneta)
+print(bicicleta)
